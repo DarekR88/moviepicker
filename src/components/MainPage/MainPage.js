c47af8490e45ecc94929a06c9f335a0bd9016ce6
Darek Radke <dradmclad@gmail.com>
2022-08-14 21:53:46 -0500

    main page set up

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MainPage.scss";
+
+const MaimPage = (props) => {
+  return (
+    <div className="main-page">
+ 
+    </div>
+  );
+};
+
+export default MaimPage;
