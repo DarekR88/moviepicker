f621d0b44053866101806ae8f51a51ad02552235
Darek Radke <dradmclad@gmail.com>
2022-08-15 17:05:32 -0500

    random movie component created

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { testSearch } from "../../API/index.js";
 import "./MainPage.scss";
 
 const MaimPage = (props) => {
-  const [randomSearch, setRandomSearch] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    testSearch(Math.floor(Math.random() * (2022 - 1939) + 1939)).then((res) => {
-      console.log(res.data.results[0]);
-    });
-  });
 
   return (
     <div className="main-page">
---
+++ src/components/RandomMovie/RandomMovie.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { testSearch } from "../../API/index.js";
+import "./RandomMovie.scss"
+
+const RandomMovie = (props) => {
+  const [randomSearch, setRandomSearch] = useState("");
+
+  useEffect(() => {
+    testSearch(Math.floor(Math.random() * (2022 - 1939) + 1939)).then((res) => {
+      setRandomSearch(res.data.results[0]);
+    });
+  }, []);
+
+  return (
+    <div className="random-movie-wrapper">
+      <div className="random-movie">
+        <p>Random Movie</p>
+      </div>
+    </div>
+  );
+};
+
+export default RandomMovie;
