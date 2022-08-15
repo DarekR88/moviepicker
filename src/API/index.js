220fed72fe0534e081129f2a560d642321591f20
Darek Radke <dradmclad@gmail.com>
2022-08-15 16:30:40 -0500

    returning data in the correct component

--- src/API/index.js
+++ src/API/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const testSearch = (year) => {
-  axios
+  return axios
     .get(
       `https://api.themoviedb.org/3/discover/movie?api_key=805e6e3607d3f594b155862e71830dd2&year=${year}`
     )
-    .then((res) => {
-      console.log(res.data.results[0]);
-    });
+    // .then((res) => {
+    //   console.log(res.data.results[0]);
+    // });
 };
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { testSearch } from "../../API/index.js";
 import "./MainPage.scss";
 
 const MaimPage = (props) => {
+  const [randomSearch, setRandomSearch] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    testSearch(Math.floor(Math.random() * (2022 - 1939) + 1939));
+    testSearch(Math.floor(Math.random() * (2022 - 1939) + 1939)).then((res) => {
+      console.log(res.data.results[0]);
+    });
   });
 
   return (
@@ -13,9 +17,8 @@ const MaimPage = (props) => {
       <div className="mp-title">
         <p>What to Watch?</p>
       </div>
-    </div> 
-  )
-
+    </div>
+  );
 };
 
 export default MaimPage;
