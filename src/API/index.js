7e28f18f6ca82ea7d7dd3aa36d4881db6321c9dd
Darek Radke <dradmclad@gmail.com>
2022-08-16 00:18:02 -0500

    movie poster in random movie

--- src/API/index.js
+++ src/API/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const testSearch = (year) => {
   return axios
     .get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=805e6e3607d3f594b155862e71830dd2&year=${year}&sort_by=popularity.desc`
+      `https://api.themoviedb.org/3/discover/movie?api_key=805e6e3607d3f594b155862e71830dd2&year=${year}&language=en-US&sort_by=popularity.desc`
     )
 };
 
--- src/components/RandomMovie/RandomMovie.js
+++ src/components/RandomMovie/RandomMovie.js
@@ -14,6 +14,7 @@ const RandomMovie = (props) => {
   return (
     <div className="random-movie-wrapper">
       <p>Random Movie</p>
+      <img src={`http://image.tmdb.org/t/p/w500${randomSearch.poster_path}`}/>
       <p>Title: {randomSearch.original_title}</p>
       <p>Synopsis: {randomSearch.overview}</p>
       <p>Release Date: {randomSearch.release_date}</p>
