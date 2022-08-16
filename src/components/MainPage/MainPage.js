2ea2540ead8f42768ad0695b12ea816d6bf35e27
Darek Radke <dradmclad@gmail.com>
2022-08-15 23:46:43 -0500

    search form

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,10 +5,23 @@ import RandomMovie from "../RandomMovie/RandomMovie";
 const MaimPage = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchTerm)
+  }
+
   return (
     <div className="main-page">
       <div className="mp-title">
         <p>What to Watch?</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} />
+          <input type="submit" value="Search" />
+        </form>
         <RandomMovie />
       </div>
     </div>
