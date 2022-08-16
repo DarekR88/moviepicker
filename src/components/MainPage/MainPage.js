5dea44e50fe9d8c8a8cbc867c7a67c88e8f178d2
Darek Radke <dradmclad@gmail.com>
2022-08-16 00:44:19 -0500

    pushing multiSearch results to state

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./MainPage.scss";
 import RandomMovie from "../RandomMovie/RandomMovie";
+import { multiSearch } from "../../API";
 
 const MaimPage = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [multiRsults, setMultiResults] = useState("");
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -11,7 +13,9 @@ const MaimPage = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(searchTerm);
+    multiSearch(searchTerm).then((res) => {
+      setMultiResults(res.data.results);
+    });
   };
 
   return (
